65648a54f775a59aaf4ac0ff77e6328bb80c2b6b
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-15 03:03:18 +0600

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -14,7 +14,7 @@ export default function About() {
         <p className="text-lg mb-6 text-gray-700 leading-7">
           With a deep understanding of the local market, we help you find the perfect place to call home in the most desirable neighborhoods.
         </p>
-<p className="text-lg mb-6 text-gray-700 leading-7">
+        <p className="text-lg mb-6 text-gray-700 leading-7">
           At PrimeEstatePRO, we believe in turning your dreams into reality. Contact us today to get started on your real estate journey.
         </p>
       </div>
@@ -29,3 +29,11 @@ export default function About() {
           <p className="text-lg mb-6 text-gray-700 leading-7">
             Meet our dedicated team of professionals who are here to guide you every step of the way.
           </p>
+          <button className="bg-gray-800 text-white px-6 py-2 rounded-full font-bold hover:bg-gray-900 transition duration-300">
+            Learn More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
