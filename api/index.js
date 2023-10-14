23c0efc920b4650a34c20683d0148f0031a7a46b
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-14 22:54:30 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,10 @@
 
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
