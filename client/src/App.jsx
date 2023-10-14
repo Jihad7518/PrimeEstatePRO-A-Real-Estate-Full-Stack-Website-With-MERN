b0d8c8ad4ba88b8cbf02b8c1214c50076c7b2d2a
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-15 02:59:46 +0600

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,3 +12,20 @@ import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
 import Contact from './pages/Contact';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path='/listing/:listingId' element={<Listing />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
