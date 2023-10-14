806b77e5b3b8ec5f9b98345ec71b17ba14c04a82
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-15 03:02:50 +0600

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
 
+export default function About() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto text-center">
+      <div className="max-w-2xl mx-auto mb-16">
+        <h1 className="text-4xl font-bold mb-8 text-gray-800">Welcome to PrimeEstatePRO</h1>
+        <p className="text-lg mb-6 text-gray-700 leading-7">
+          PrimeEstatePRO is your trusted partner in real estate. Our experienced team is committed to providing personalized service and expert advice throughout your real estate journey.
+        </p>
+        <p className="text-lg mb-6 text-gray-700 leading-7">
+          Whether you're buying, selling, or renting, we're here to make the process seamless and rewarding.
+        </p>
+        <p className="text-lg mb-6 text-gray-700 leading-7">
+          With a deep understanding of the local market, we help you find the perfect place to call home in the most desirable neighborhoods.
+        </p>
