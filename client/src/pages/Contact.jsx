2ef1d4969c7201932bab3f63ff5c61784df2f254
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-23 00:06:25 +0600

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -25,3 +25,42 @@ export default function ContactPage() {
     const mailtoLink = `mailto:jihadmd7518@gmail.com?subject=${subject}&body=${body}`;
     window.location.href = mailtoLink;
   };
+return (
+    <div className="max-w-3xl mx-auto mt-10 p-5">
+      <h1 className="text-2xl font-bold mb-5">Contact Us</h1>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label htmlFor="name" className="block font-bold mb-1">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="w-full border border-gray-300 p-2 rounded"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="block font-bold mb-1">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="w-full border border-gray-300 p-2 rounded"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="message" className="block font-bold mb-1">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            rows="5"
+            className="w-full border border-gray-300 p-2 rounded"
+            required
+          ></textarea>
