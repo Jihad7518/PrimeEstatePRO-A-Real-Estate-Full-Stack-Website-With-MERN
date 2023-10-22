2a855dd304beae0792290298e1353fb793c8f3bd
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-23 00:06:05 +0600

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,1 +1,27 @@
 
+import { useState } from 'react';
+
+export default function ContactPage() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { name, email, message } = formData;
+    const subject = `Contact Form Submission from ${name}`;
+    const body = `Name: ${name}%0D%0AEmail: ${email}%0D%0AMessage: ${message}`;
+
+    const mailtoLink = `mailto:jihadmd7518@gmail.com?subject=${subject}&body=${body}`;
+    window.location.href = mailtoLink;
+  };
