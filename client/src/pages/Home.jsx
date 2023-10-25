56aa526e8319065df66f6316e6cff0609741cd09
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-25 23:48:53 +0600

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -47,3 +47,50 @@ const fetchRentListings = async () => {
 
     fetchOfferListings();
   }, []);
+
+  return (
+    <div>
+      {/* Swiper */}
+      <Swiper navigation Autoplay={{ delay: 0 }}>
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings.map((listing) => (
+            <SwiperSlide key={listing._id}>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+                className="h-[500px]"
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/* Hero Section */}
+      <section
+        className="text-white py-20 text-center"
+        style={{
+          backgroundColor: 'rgba(0, 0, 0, 0)', // Adjust opacity as needed
+          position: 'absolute',
+          width: '100%',
+          height: '65%',
+          top: 100,
+          left: 0,
+        }}
+      >
+        <div className="container mx-auto px-4 relative z-10">
+          <h1 className="text-4xl lg:text-6xl font-bold mb-5 mt-1 pt-1">
+            Find Your Dream Home
+          </h1>
+          <p className="text-lg mb-8">
+            Discover a wide range of properties to choose from.
+          </p>
+          <Link
+            to={'/search'}
+            className="bg-white text-blue-700 py-3 px-8 font-bold rounded-full transition duration-300 hover:bg-blue-800 hover:text-white"
+          >
+            Get Started
+          </Link>
+        </div>
+      </section>
