76822bcdcd60cfb5a90a7db34db2e71bc620b43f
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-26 01:14:15 +0600

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -258,3 +258,28 @@ const handleSignOut = async () => {
                   className='h-16 w-16 object-contain'
                 />
               </Link>
+<Link
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
