5e870df665966f7430e4bcda6ded4c6b37d31e5b
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-27 00:24:46 +0600

    Update UpdateListing.jsx

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -263,3 +263,53 @@ export default function CreateListing() {
               onChange={handleChange}
               value={formData.bedrooms}
             />
+<p>Beds</p>
+          </div>
+          <div className='flex items-center gap-2'>
+            <input
+              type='number'
+              id='bathrooms'
+              min='1'
+              max='10'
+              required
+              className='w-20 p-3 border border-gray-300 rounded-lg'
+              onChange={handleChange}
+              value={formData.bathrooms}
+            />
+            <p>Baths</p>
+          </div>
+          <div className='flex items-center gap-2'>
+            <input
+              type='number'
+              id='regularPrice'
+              min='50'
+              max='10000000'
+              required
+              className='w-32 p-3 border border-gray-300 rounded-lg'
+              onChange={handleChange}
+              value={formData.regularPrice}
+            />
+            <div className='flex flex-col items-center'>
+              <p>Regular price</p>
+              <span className='text-xs'>($ / month)</span>
+            </div>
+          </div>
+          {formData.offer && (
+            <div className='flex items-center gap-2'>
+              <input
+                type='number'
+                id='discountPrice'
+                min='0'
+                max='10000000'
+                required
+                className='w-32 p-3 border border-gray-300 rounded-lg'
+                onChange={handleChange}
+                value={formData.discountPrice}
+              />
+              <div className='flex flex-col items-center'>
+                <p>Discounted price</p>
+                <span className='text-xs'>($ / month)</span>
+              </div>
+            </div>
+          )}
+        </div>
