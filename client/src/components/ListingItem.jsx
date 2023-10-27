4247dcd262f03ed7ce71bd6c6afdf82869ce2cef
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-28 00:35:43 +0600

    Update ListingItem.jsx

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -24,3 +24,27 @@ export default function ListingItem({ listing }) {
               {listing.address}
             </p>
           </div>
+<p className='text-sm text-gray-600 line-clamp-2'>
+            {listing.description}
+          </p>
+          <p className='text-slate-500 mt-2 font-semibold '>
+            $
+            {listing.offer
+              ? listing.discountPrice.toLocaleString('en-US')
+              : listing.regularPrice.toLocaleString('en-US')}
+            {listing.type === 'rent' && ' / month'}
+          </p>
+          <div className='text-slate-700 flex gap-4'>
+            <div className='font-bold text-xs'>
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} beds `
+                : `${listing.bedrooms} bed `}
+            </div>
+            <div className='font-bold text-xs'>
+              {listing.bathrooms > 1
+                ? `${listing.bathrooms} baths `
+                : `${listing.bathrooms} bath `}
+            </div>
+          </div>
+        </div>
+      </Link>
