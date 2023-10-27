4b9dfd8361ee6e3571cffd74431b0983a1982069
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-28 00:36:24 +0600

    Update OAuth.jsx

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,4 +1,3 @@
-
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
@@ -26,3 +25,20 @@ export default function OAuth() {
           photo: result.user.photoURL,
         }),
       });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      console.log('could not sign in with google', error);
+    }
+  };
+  return (
+    <button
+      onClick={handleGoogleClick}
+      type='button'
+      className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+    >
+      Continue with google
+    </button>
+  );
+}
