455bc996d0f0d93d6e8e551c0dfd6c8be425770c
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-10-28 00:34:37 +0600

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -16,3 +16,31 @@ export default function Header() {
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
   };
+useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <header className='bg-gray-900 text-white shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold text-xl'>
+            <span className='text-blue-500'>PrimeEstate</span>
+            <span className='text-blue-300'>PRO</span>
+          </h1>
+        </Link>
+        <form
+          onSubmit={handleSubmit}
+          className='bg-white p-3 rounded-lg flex items-center shadow-sm'
+        >
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64 text-gray-900'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
